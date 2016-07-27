4b71051e78739cb83063b1244a225a7c5821ece0
oeyh <yanhai12345@gmail.com>
2016-07-27 16:50:26 -0500

    res can be either a number or an array

--- TestBench/peakfit.m
+++ TestBench/peakfit.m
@@ -1,12 +1,11 @@
 function peak = peakfit(spectrumData, res, sWidth, fitting, fWidth)
-%peakfit takes spectrum data and makes Lorentzian fit
+%peakfit takes spectrum data and makes Lorentzian fit and find peaks
 %   spectrumData is a N-by-2 matrix containing wavelength and power data
-%   res is estimated resonance position
+%   res is a number or an array of estimated resonance position
 %   fitting indicates applying fitting(t) or simply finding the minimum(f)
 %   sWidth is the half range for searching minimum
 %   fWidth is the fitting half range, +/- halfwidth
-%   returns peak, wavelength of the minimum point
-%   and peakFit, wavelength of the fitted minimum
+%   returns peak, a number or an array of wavelength of the minimum point
     
     if nargin < 5
         fWidth = 0;
@@ -21,30 +20,33 @@ function peak = peakfit(spectrumData, res, sWidth, fitting, fWidth)
     
     % find local minimum
     lambda = spectrumData(:,1);
-    minSearch = res - sWidth;
-    maxSearch = res + sWidth;
-    searchRange = (lambda > minSearch) & ...
-                    (lambda < maxSearch);
-    M = spectrumData(searchRange, :);
-    [~, I] = min(M(:, 2));
-    peak = M(I, 1);
+    minSearch = res - sWidth;   % array
+    maxSearch = res + sWidth;   % array
+    n = length(res);
+    peak = zeros(1,n);
+    for i = 1:n
+        searchRange = (lambda > minSearch(i)) & ...
+                        (lambda < maxSearch(i));
+        M = spectrumData(searchRange, :);
+        [~, I] = min(M(:, 2));
+        peak(i) = M(I, 1);
 
-    % Check if local minimum is out of range because of bad data
-    if (peak > upLimit) || (peak < lowLimit)
-        peak = res;
-        disp('peak at edge')
-    end
-    
-    if fitting == true
-        % apply Lorentzian fit around the above minimum
-        fitRange = (lambda > peak - fWidth) & ...
-                        (lambda < peak + fWidth);
-        M2 = spectrumData(fitRange, :);
-        x = M2(:, 1);
-        y = M2(:, 2);
-        [~, params , ~, ~] = lorentzfit(x,y);
-        peak = params(2); % params(2) is the exact center of the fitting
-    end
+        % Check if local minimum is out of range because of bad data
+        if (peak(i) > upLimit) || (peak(i) < lowLimit)
+            peak(i) = res(i);
+            disp('peak at edge')
+        end
 
+        if fitting == true
+            % apply Lorentzian fit around the above minimum
+            fitRange = (lambda > peak(i) - fWidth) & ...
+                            (lambda < peak(i) + fWidth);
+            M2 = spectrumData(fitRange, :);
+            x = M2(:, 1);
+            y = M2(:, 2);
+            [~, params , ~, ~] = lorentzfit(x,y);
+            peak(i) = params(2); % params(2) is the exact center of the fitting
+        end
+    end
 end
 
